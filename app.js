eee3e20ec2d7ab6fcc73607c1c7e5c7f82892a7d
Filipa Silveira <filipasilveira2016@outlook.pt>
2020-05-08 15:31:22 +0100

    Silveira

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 const express = require('express');
-const app = express();
+//const expressLayouts = require('express-ejs-layouts');
+//const session = require('express-session');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const app = express();
+
 app.use(bodyParser.json());
-
-//import routes
-const adminRoute = require('./routes/admin');
-const homeRoute = require('./routes/home');
-const tecnicosRoute = require('./routes/tecnicos');
-
-app.use('/tecnicos', tecnicosRoute);
-app.use('/', homeRoute);
-app.use('/admin', adminRoute);
 
 //Connect DB
 mongoose.connect('mongodb+srv://trabalhopaw:paw2020@cluster0-gifs8.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, () =>
     console.log('connected to db')
 );
+
+
+
+
+
+//import routes
+const homeRoute = require('./routes/home');
+const adminRoute = require('./routes/admin');
+const tecnicosRoute = require('./routes/tecnicos');
+
+app.use('/', homeRoute);
+app.use('/admin', adminRoute);
+app.use('/tecnicos', tecnicosRoute);
+
 
 //Listen 
 app.listen(3000);
